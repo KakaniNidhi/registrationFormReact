c454370ceba0d95c8fba7655a0628d7187a0fe46
KakaniNidhi <nidhi.kakani1234@gmail.com>
2022-03-27 19:07:40 +0530

    React Native app demo success.

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
